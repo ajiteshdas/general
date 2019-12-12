f419c8a46046646fcdce01ffb742f847756d3898
ajiteshdas <52836750+ajiteshdas@users.noreply.github.com>
2019-12-12 19:03:44 +0530

    get 5 premium and discount arbitrage

--- cash_carry_arbitrage.py
+++ cash_carry_arbitrage.py
@@ -39,8 +39,30 @@ d = pd.DataFrame(d)
 stocks = pd.DataFrame(stocks)
 futures = pd.concat([stocks,d], axis=1)
 
-futures['stocks'] = stocks
-futures['futures_arb'] = d
+top_arbs_stocks = []
+top_arbs_percents = []
+bottom_arbs_stocks = []
+bottom_arbs_percents = []
 
+#get the top and bottom 5 arbitrages
+num_arbs = 5
+print('Arbitrage Premium:')
+for i in range(len(futures['futures_arb'].nlargest(num_arbs).keys())):
+    top_arbs_stocks.append(futures['stocks'][futures['futures_arb'].nlargest(5).keys()[i]])
+    top_arbs_percents.append('{:.2f}'.format(futures['futures_arb'][futures['futures_arb'].nlargest(5).keys()[i]])+'%')
+top_arbs_stocks = pd.DataFrame(top_arbs_stocks)
+top_arbs_percents = pd.DataFrame(top_arbs_percents)
+top_arbs = pd.concat([top_arbs_stocks,top_arbs_percents], axis=1)
+print(top_arbs)
+
+print('\n****************************\n')
+print('Arbitrage Discount:')
+for i in range(len(futures['futures_arb'].nsmallest(num_arbs).keys())):
+    bottom_arbs_stocks.append(futures['stocks'][futures['futures_arb'].nsmallest(5).keys()[i]])
+    bottom_arbs_percents.append('{:.2f}'.format(futures['futures_arb'][futures['futures_arb'].nsmallest(5).keys()[i]])+'%')
+bottom_arbs_stocks = pd.DataFrame(bottom_arbs_stocks)
+bottom_arbs_percents = pd.DataFrame(bottom_arbs_percents)
+bottom_arbs = pd.concat([bottom_arbs_stocks,bottom_arbs_percents], axis=1)
+print(bottom_arbs)
 #get the maximum cash and carry arb: stock name and % returns
-print(futures['stocks'][futures['futures_arb'].idxmax()]+':', '{:.2f}'.format(futures['futures_arb'][futures['futures_arb'].idxmax()])+'%')
+#print(futures['stocks'][futures['futures_arb'].idxmax()]+':', '{:.2f}'.format(futures['futures_arb'][futures['futures_arb'].idxmax()])+'%')
