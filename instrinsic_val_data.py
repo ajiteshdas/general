c8600e472bbf677c868d4f5813db4f555f55b2b0
ajiteshdas <52836750+ajiteshdas@users.noreply.github.com>
2020-02-03 12:30:05 +0530

    DRY codes

--- instrinsic_val_data.py
+++ instrinsic_val_data.py
@@ -6,146 +6,87 @@ import pandas as pd
 import os
 
 stocks_data = []
+urlValue = ''
+tableSelected = ''
 
-cashflow_operating_2019 = []
-cashflow_operating_2018 = []
-cashflow_operating_2017 = []
-cashflow_operating_2016 = []
+#whichData = 'DEP'
+#whichData = 'CFo' 
+whichData = 'Capex'
+#whichData = 'div'
+#whichData = 'cAssets' 
+#whichData = 'cLiab' 
+#whichData = 'lDebt'
+#whichData = 'EBIT' 
+#whichData = 'Rev' 
+#whichData = 'COGS'
+#whichData = 'netInc'
+#whichData = 'int'
 
-cash_cash_eq_2019 = []
-cash_cash_eq_2018 = []
-cash_cash_eq_2017 = []
-cash_cash_eq_2016 = []
+if (whichData in ['DEP', 'CFo', 'Capex','div','cash']):
+    if (whichData == 'DEP'):
+        tableSelected = 16
+    elif (whichData == 'CFo'):
+        tableSelected = 79
+    elif (whichData == 'Capex'):
+        tableSelected = 213
+    elif (whichData == 'cash'):
+        tableSelected = 19
+    elif (whichData == 'div'):
+        tableSelected = 161
+    urlValue = 'cash-flow'
+    print('Casflow selected: ' + whichData)
 
-current_asset_liabilities_2019 = []
-current_asset_liabilities_2018 = []
-current_asset_liabilities_2017 = []
-current_asset_liabilities_2016 = []
+elif (whichData in ['cAssets', 'cLiab', 'lDebt']):
+    if (whichData == 'cAssets'):
+        tableSelected = 55
+    elif (whichData == 'cLiab'):
+        tableSelected = 180
+    elif (whichData == 'lDebt'):
+        tableSelected = 192
+    urlValue = 'balance-sheet'
+    print('Balance sheet selected: ' + whichData)
 
-div_2019 = []
-div_2018 = []
-div_2017 = []
-div_2016 = []
+elif (whichData in ['EBIT', 'Rev', 'COGS','netInc','int']):
+    if (whichData == 'EBIT'):
+        tableSelected = 47
+    elif (whichData == 'Rev'):
+        tableSelected = 2
+    elif (whichData == 'COGS'):
+        tableSelected = 8
+    elif (whichData == 'netInc'):
+        tableSelected = 89
+    elif (whichData == 'int'):
+        tableSelected = 53
+    urlValue = 'financials'
+    print('Income statement selected: ' + whichData)
 
-long_term_debt_2019 = []
-long_term_debt_2018 = []
-long_term_debt_2017 = []
-long_term_debt_2016 = []
-
-capex_2019 = []
-capex_2018 = []
-capex_2017 = []
-capex_2016 = []
-
-interest_2019 = []
-interest_2018 = []
-interest_2017 = []
-interest_2016 = []
-
-oper_inc_2019 = []
-oper_inc_2018 = []
-oper_inc_2017 = []
-oper_inc_2016 = []
+data_current = []
+data_prev1 = []
+data_prev2 = []
+data_prev3 = []
 
 no_data = []
-
-stocks = ['AAA','BBB','CCC']
+#stocks = ['AAA','BBB']
 
 def get_data():
     for stock in stocks:
-
         #use only one url at a time as Yahoo starts blocking if too much traffic
-        #cashflow_url = 'https://finance.yahoo.com/quote/' + stock + '/cash-flow?p=' + stock
-        #balance_sheet_url = 'https://finance.yahoo.com/quote/' + stock + '/balance-sheet?p=' + stock
-        income_statement_url = 'https://finance.yahoo.com/quote/' + stock + '/financials?p=' + stock
+        url = 'https://finance.yahoo.com/quote/' + stock + '/'+ urlValue + '?p=' + stock
         # Fetch the page that we're going to parse
-        #cashflow_page = requests.get(cashflow_url)
-        #bs_page = requests.get(balance_sheet_url)
-        income_page = requests.get(income_statement_url)
+        page = requests.get(url)
         # Parse the page with LXML, so that we can start doing some XPATH queries
         # to extract the data that we want
-        #cashflow_tree = html.fromstring(cashflow_page.content)
-        #bs_tree = html.fromstring(bs_page.content)
-        inc_tree = html.fromstring(income_page.content)
-
+        tree = html.fromstring(page.content)
         # Using XPATH, fetch all table elements on the page
-        #cashflow_table = cashflow_tree.xpath('//div[@data-test="fin-row"]/div/div')
-        #bs_table = bs_tree.xpath('//div[@data-test="fin-row"]/div/div')
-        income_table = inc_tree.xpath('//div[@data-test="fin-row"]/div/div')
+        table = tree.xpath('//div[@data-test="fin-row"]/div/div')
 
-        if (len(income_table)>=80):
+        if (len(table)>=tableSelected-1):
             stocks_data.append(stock)
-            #forCashflowOperations
-            '''cashflow_operating_2019.append(cashflow_table[79].text_content())
-            cashflow_operating_2018.append(cashflow_table[80].text_content())
-            cashflow_operating_2017.append(cashflow_table[81].text_content())
-            cashflow_operating_2016.append(cashflow_table[82].text_content())'''
 
-            #forCapex
-            '''capex_2019.append(cashflow_table[213].text_content())
-            capex_2018.append(cashflow_table[214].text_content())
-            capex_2017.append(cashflow_table[215].text_content())
-            capex_2016.append(cashflow_table[216].text_content())'''
-
-            #forCashandCashEq
-            '''cash_cash_eq_2019.append(bs_table[19].text_content())
-            cash_cash_eq_2018.append(bs_table[20].text_content())
-            cash_cash_eq_2017.append(bs_table[21].text_content())
-            cash_cash_eq_2016.append(bs_table[22].text_content())'''
-
-            #forCurrentAssets
-            '''current_asset_liabilities_2019.append(bs_table[55].text_content())
-            current_asset_liabilities_2018.append(bs_table[56].text_content())
-            current_asset_liabilities_2017.append(bs_table[57].text_content())
-            current_asset_liabilities_2016.append(bs_table[58].text_content())'''
-
-            #forCurrentLiabilities
-            '''current_asset_liabilities_2019.append(bs_table[180].text_content())
-            current_asset_liabilities_2018.append(bs_table[181].text_content())
-            current_asset_liabilities_2017.append(bs_table[182].text_content())
-            current_asset_liabilities_2016.append(bs_table[183].text_content())'''
-
-            #forLONGTERMDEBT
-            '''long_term_debt_2019.append(bs_table[192].text_content())
-            long_term_debt_2018.append(bs_table[193].text_content())
-            long_term_debt_2017.append(bs_table[194].text_content())
-            long_term_debt_2016.append(bs_table[195].text_content())'''
-
-            #forOperIncome
-            '''oper_inc_2019.append(income_table[47].text_content())
-            oper_inc_2018.append(income_table[48].text_content())
-            oper_inc_2017.append(income_table[49].text_content())
-            oper_inc_2016.append(income_table[50].text_content())'''
-
-            #forTotalRevenues
-            '''oper_inc_2019.append(income_table[2].text_content())
-            oper_inc_2018.append(income_table[3].text_content())
-            oper_inc_2017.append(income_table[4].text_content())
-            oper_inc_2016.append(income_table[5].text_content())'''
-
-            #forCostofRevenue
-            '''oper_inc_2019.append(income_table[8].text_content())
-            oper_inc_2018.append(income_table[9].text_content())
-            oper_inc_2017.append(income_table[10].text_content())
-            oper_inc_2016.append(income_table[11].text_content())'''
-
-            #forNetIncome
-            oper_inc_2019.append(income_table[89].text_content())
-            oper_inc_2018.append(income_table[90].text_content())
-            oper_inc_2017.append(income_table[91].text_content())
-            oper_inc_2016.append(income_table[92].text_content())
-
-            #forInterest
-            '''interest_2019.append(income_table[53].text_content())
-            interest_2018.append(income_table[54].text_content())
-            interest_2017.append(income_table[55].text_content())
-            interest_2016.append(income_table[56].text_content())'''
-
-            #forDividendsPaid
-            '''div_2019.append(cashflow_table[161].text_content())
-            div_2018.append(cashflow_table[162].text_content())
-            div_2017.append(cashflow_table[163].text_content())
-            div_2016.append(cashflow_table[164].text_content())'''
+            data_current.append(table[tableSelected].text_content())
+            data_prev1.append(table[tableSelected+1].text_content())
+            data_prev2.append(table[tableSelected+2].text_content())
+            data_prev3.append(table[tableSelected+3].text_content())
 
         else:
             print(stock)
@@ -156,29 +97,8 @@ get_data()
 no_data_df = pd.DataFrame(no_data)
 no_data_df.to_csv('no_data.csv')
 
-cashflows = {'Name':stocks_data,'2019':cashflow_operating_2019, '2018':cashflow_operating_2018,'2017':cashflow_operating_2017, '2016':cashflow_operating_2016}
+dictionaryCreate = {'Name':stocks_data,'current':data_current, 'prev1':data_prev1,'prev2':data_prev2, 'prev3':data_prev3}
 
-capexs = {'Name':stocks_data,'2019':capex_2019, '2018':capex_2018, '2017':capex_2017, '2016':capex_2016}
+dataframeCreate = pd.DataFrame(dictionaryCreate)
 
-cash_cash_eqs = {'Name':stocks_data,'2019':cash_cash_eq_2019, '2018':cash_cash_eq_2018, '2017':cash_cash_eq_2017, '2016':cash_cash_eq_2016}
-
-long_term_debts = {'Name':stocks_data,'2019':long_term_debt_2019, '2018':long_term_debt_2018, '2017':long_term_debt_2017, '2016':long_term_debt_2016}
-
-interests = {'Name':stocks_data,'2019':interest_2019, '2018':interest_2018, '2017':interest_2017, '2016':interest_2016}
-
-op_incs = {'Name':stocks_data,'2019':oper_inc_2019, '2018':oper_inc_2018, '2017':oper_inc_2017, '2016':oper_inc_2016}
-
-dividends= {'Name':stocks_data,'2019':div_2019, '2018':div_2018, '2017':div_2017, '2016':div_2016}
-
-assets_liabilities= {'Name':stocks_data,'2019':current_asset_liabilities_2019, '2018':current_asset_liabilities_2018, '2017':current_asset_liabilities_2017, '2016':current_asset_liabilities_2016}
-
-#cashflow = pd.DataFrame(cashflows)
-#capex = pd.DataFrame(capexs)
-#cash_cash_eq = pd.DataFrame(cash_cash_eqs)
-#long_term_debt = pd.DataFrame(long_term_debts)
-oper_inc = pd.DataFrame(op_incs)
-#interest = pd.DataFrame(interests)
-#dividend = pd.DataFrame(dividends)
-#assets_liablity = pd.DataFrame(assets_liabilities)
-
-pd.DataFrame(op_incs).to_csv('data.csv')
+pd.DataFrame(dataframeCreate).to_csv('data.csv')
